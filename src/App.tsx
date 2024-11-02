0c321e53375cf5f0738f938ee78e2eb25cfc9b38
Keith Sialana <ninjabasketball@hotmail.com>
2024-11-02 16:19:57 -0400

    added navigation buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import Nav from './components/Nav';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function App() {
   return (
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,36 @@
+import { Link, useLocation } from "react-router-dom";
+
 const Nav = () => {
-  // TODO: Add necessary code to display the navigation bar and link between the pages
-  return (
-    <div>Nav</div>
-  )
+	// Add necessary code to display the navigation bar and link between the pages
+	const currentLocation = useLocation().pathname;
+	return (
+		<div className="container-fluid w-100">
+			<div className="w-100">
+				<Link to="/">
+					{currentLocation == "/" ? (
+						<div className="btn btn-outline-primary active" aria-current="page">
+							Candidate Search
+						</div>
+					) : (
+						<div className="btn btn-outline-primary" aria-current="page">
+							Candidate Search
+						</div>
+					)}
+				</Link>
+				<Link to="/SavedCandidates">
+					{currentLocation == "/SavedCandidates" ? (
+						<div className="btn btn-outline-primary active" aria-current="page">
+							Saved Candidates
+						</div>
+					) : (
+						<div className="btn btn-outline-primary" aria-current="page">
+							Saved Candidates
+						</div>
+					)}
+				</Link>
+			</div>
+		</div>
+	);
 };
 
 export default Nav;
